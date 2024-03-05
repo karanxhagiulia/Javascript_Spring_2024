d9fe6abb6ec880087afbe51b3061c8551d12fde7
Your Name <karanxhagiulia@gmail.com>
2024-03-05 18:25:58 +0100

    new

--- js/shopping.js
+++ js/shopping.js
@@ -1,9 +1,11 @@
 function calculate() {
-    var quantity = parseInt(document.getElementById("quantity").value);
+
+    var quantity = parseInt(document.getElementById("quantityex3").value);
     var price = parseFloat(document.getElementById("price").value);
     var tax = parseFloat(document.getElementById("tax").value);
-    var discount = parseFloat(document.getElementById("discount").value);
+    var discount = parseFloat(document.getElementById("discountex3").value); // I had to call it "discountex3" because it was taking the id discount above, causing a NaN issue in the Total 
     var shipping = parseFloat(document.getElementById("shipping").value);
+
     // Apply discount if quantity is greater than 100
     if (quantity > 100) {
         discount *= 2;
